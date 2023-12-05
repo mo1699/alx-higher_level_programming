12d02617967f999a373d3a71d0ffdee75e825fe6
mo1699 <mo1699@gmail.com>
2023-12-05 22:22:52 +0200

    C - Hello, World

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - Godswill Kalu
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+for i in range(97, 123):
+    if i !=  101 and  != 113:
+        print("{}".format(chr(i)), end="")
