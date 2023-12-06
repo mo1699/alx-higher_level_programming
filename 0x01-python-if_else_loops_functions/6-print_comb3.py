d07b0ad7221896850ccb06b9d303caa6e15c8c8b
mo1699 <mo1699@gmail.com>
2023-12-06 02:54:31 +0200

    C - Hello, World

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for e in range(i+1 ,10):
+    for e in range(i + 1 , 10):
         if i != 8 and e != 9:
             print("{}{}".format(i, e), end=", ")
         else:
