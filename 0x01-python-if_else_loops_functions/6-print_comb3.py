68a50eaacf311805e7216ef7483de05af8fb18fb
mo1699 <mo1699@gmail.com>
2023-12-06 02:52:01 +0200

    C - Hello, World

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
-
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for i in range(0, 10):
+    for e in range(i+1 ,10):
+        if i != 8 and e != 9:
+            print("{}{}".format(i, e), end=", ")
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(i, e))
