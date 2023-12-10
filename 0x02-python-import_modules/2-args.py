070e0431de19640005a0f6cea376545227cc56ef
mo1699 <mo1699@gmail.com>
2023-12-10 19:42:15 +0200

    C - Hello, World

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,7 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+        print("{} arguments.".format(count))
     else:
         print("{} arguments:".format(count))
     for i in range(count):
