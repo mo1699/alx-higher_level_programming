26b53c5b550ed8082139a97bcd150838b04d0a8d
mo1699 <mo1699@gmail.com>
2024-01-26 06:59:48 +0200

    C - Hello, World

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) < 1:
-        return None:
+        return None
     copy = my_list.copy()
     copy.sort()
     return copy[-1]
