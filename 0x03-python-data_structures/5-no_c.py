6b0be0c47a807afd2bb8b5a1e27790a3be969482
mo1699 <mo1699@gmail.com>
2024-01-26 06:03:52 +0200

    C - Hello, World

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for i in range(len(my_string)):
         if (my_string[i] != 'c' and my_string[i] != 'C'):
             res += my_string[i]
-            return res
+    return res
