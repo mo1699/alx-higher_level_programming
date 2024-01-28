a0d24d1087ecb7190e9bd3c56cb254127d28777e
mo1699 <mo1699@gmail.com>
2024-01-29 00:55:19 +0200

    C - Hello, World

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,4 +6,4 @@ def search_replace(my_list, search, replace):
     A function that replaces all occurrences
     of an element by another in a new list
     """
-    return list(map(lambda i : replace if i == search else i, my_list))
+    return list(map(lambda i: replace if i == search else i, my_list))
