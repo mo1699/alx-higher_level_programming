6bc44154d4bd8849a2e6108619834353e4911903
mo1699 <mo1699@gmail.com>
2024-01-30 07:33:42 +0200

    C - Hello, World

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -13,6 +13,4 @@ def common_elements(set_1, set_2):
         for j in set_2:
             if i == j:
                 listss.append(i)
-            else:
-                continue
     return listss
