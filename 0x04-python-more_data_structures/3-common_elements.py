8d964e92ce7eba255de87c65e685a5b51bf00ed8
mo1699 <mo1699@gmail.com>
2024-01-30 07:21:16 +0200

    C - Hello, World

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -6,4 +6,13 @@ def common_elements(set_1, set_2):
     A function that returns a set of
     common elements in two sets
     """
-    return(set_1 & set_2)
+
+    listss = []
+
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                listss.append(i)
+            else:
+                continue
+    return listss
