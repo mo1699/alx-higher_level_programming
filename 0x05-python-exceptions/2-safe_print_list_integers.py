5a7e9c8eae938b508b10b9a313fa6a00a71f22e9
mo1699 <mo1699@gmail.com>
2024-02-01 14:43:46 +0200

    C - Hello, World

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -16,6 +16,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             res += 1
         except (ValueError, TypeError):
-            break
+            None
     print("")
     return (res)
