db2705969bdc8837e3d7f79a2e19951e0586a40b
mo1699 <mo1699@gmail.com>
2024-02-01 14:47:51 +0200

    C - Hello, World

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -15,7 +15,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             res += 1
-        except (IndexError, ValueError, TypeError):
+        except (ValueError, TypeError):
             None
     print("")
     return (res)
