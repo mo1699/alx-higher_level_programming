b0bc22098c02d575479f250d08ae4ecce8daff1f
mo1699 <mo1699@gmail.com>
2024-02-16 15:26:24 +0200

    C - Hello, World

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,13 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
-
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an inherited instance of a_class - True.
-        Otherwise - False.
-    """
+    """Checks if an object is an inherited instance of a class."""
     return isinstance(obj, a_class) and type(obj) != a_class
